772909ae1f0c9bb29fe8f9d2417f9a5ae23319b8
Harsh Patil <harshpatil223@gmail.com>
2021-12-06 20:03:33 +0530

    basic java programs

---
+++ code/01-first-java-program/Inputs.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // System.out.print("Please Enter your Roll no. : ");
+        // int rollno = in.nextInt();
+        // System.out.println("Your Roll number is " + rollno);
+        
+        // String name = in.nextLine();
+        // System.out.println(name);
+
+        float marks = in.nextFloat();
+        System.out.println(marks);
+    }
+}
---
+++ code/01-first-java-program/Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args) {
+        // primitives data types
+        int rollno = 54;
+        char word = 'r';
+        float marks = 45.65f;
+        double largeDecimalNumbers = 456784231.8324;
+        long lagreInteger = 34567892378237191L;
+        boolean check = false;
+    }
+}
