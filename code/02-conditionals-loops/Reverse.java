ba361669187cd900005dde3ecc17e3bca63c7a98
Harsh Patil <harshpatil223@gmail.com>
2021-12-07 16:36:58 +0530

    added reverse and calculator program

---
+++ code/02-conditionals-loops/Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int n = 456789;
+
+        int ans = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println(ans);
+    }
+}
