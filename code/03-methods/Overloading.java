ae19c34b0f5549a01c4371a8b0a0e285e839becb
Harsh Patil <harshpatil223@gmail.com>
2021-12-13 20:12:01 +0530

    Functions

--- code/03-methods/Overloading.java
+++ code/03-methods/Overloading.java
@@ -1,14 +1,26 @@
 public class Overloading {
     public static void main(String[] args) {
-        fun(545);
-        fun("Harsh");
+        // fun(545);
+        // fun("Harsh");
+        int ans = sum(4, 3, 76);
+        System.out.println(ans);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
     }
 
     static void fun(int a) {
+        System.out.println("first one");
         System.out.println(a);
     }
 
     static void fun(String name) {
+        System.out.println("second one");
         System.out.println(name);
     }
 }
--- code/03-methods/VarArgs.java
+++ code/03-methods/VarArgs.java
@@ -3,7 +3,17 @@ import java.util.Arrays;
 public class VarArgs {
     public static void main(String[] args) {
         // fun();
-        multiple(3, 5, "Harsh", "Patil");
+        // multiple(3, 5, "Harsh", "Patil");
+        // demo(4,5,6,7);
+        demo("Harsh", "Shubham");
+    }
+
+    static void demo(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void demo(String ...v) {
+        System.out.println(Arrays.toString(v));
     }
 
     static void multiple(int a, int b, String ...v) {
