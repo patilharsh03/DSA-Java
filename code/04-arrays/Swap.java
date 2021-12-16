a46e3da3685aee56c5f9fad7046e53aec4deb664
Harsh Patil <harshpatil223@gmail.com>
2021-12-17 00:05:00 +0530

    Array and ArrayList code added

--- code/04-arrays/Swap.java
+++ code/04-arrays/Swap.java
@@ -3,9 +3,22 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
         int arr[] = {1, 3, 254, 22, 165};
-        swap(arr, 0, 4);
+        // swap(arr, 0, 4);
 
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            // swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
     }
     
     static void swap(int[] arr, int index1, int index2) {
